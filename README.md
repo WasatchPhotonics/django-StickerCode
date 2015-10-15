224d004dec4a963adeb866a4123f29c890968e49
nharringtonwasatch <nharrington@wasatchphotonics.com>
2015-10-15 14:22:58 -0400

    initial file population

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # StickerCode
-web service to create qr codes, short links and QL-700 shaped stickers for device serialization
+Web service to create qr codes, short links and QL-700 shaped stickers
+for device serialization.
+
+Getting Started
+---------------
+
+- cd _directory containing this file_
+
+- $VENV/bin/python setup.py develop
+
+- $VENV/bin/pserve config/development.ini
+
---
+++ stickercode/__init__.py
@@ -0,0 +1,17 @@
+""" Defines application main, routes and configuration options for the
+pyramid application.
+"""
+from pyramid.config import Configurator
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application. Check the
+    config/.ini files for more information.
+    """
+    config = Configurator(settings=settings)
+    config.include("pyramid_chameleon")
+    config.add_static_view("static", "static", cache_max_age=3600)
+    config.add_route("cal_report", "/")
+    config.add_route("view_pdf", "/view_pdf/{serial}")
+    config.add_route("view_thumbnail", "/view_thumbnail/{serial}")
+    config.scan()
+    return config.make_wsgi_app()
